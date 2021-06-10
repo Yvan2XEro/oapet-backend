f4a3642b30331276e588857dd354fa1a2252ea90
Yvan2Ero <yvankanaj@gmail.com>
2021-06-10 06:49:47 +0100

    Correction d'une erreur au niveau des urls

--- OapetFrontend/urls.py
+++ OapetFrontend/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'auth', include('security.urls'))
+    path('auth/', include('security.urls'))
 ]
