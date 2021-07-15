ae0cf5da87145b26060d3e4e45908e63a20ebd05
Yvan2Ero <yvankanaj@gmail.com>
2021-07-15 22:44:07 +0100

    Renomage des references de OapetFrontend par OapetBackend

--- OapetBackend/wsgi.py
+++ OapetBackend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for OapetFrontend project.
+WSGI config for OapetBackend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OapetFrontend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OapetBackend.settings')
 
 application = get_wsgi_application()
